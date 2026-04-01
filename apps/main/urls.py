983db565440a9a401cdcceaacb588528a31d8b00
Levent <levozturk@hotmail.com>
2026-04-01 15:17:19 +0500

    ul il a added

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -7,6 +7,6 @@ from . import views
 #http://127.0.1:8000/
 
 urlpatterns=[
-    path("", views.render_home_main), #http://127.0.1:8000/ - home page    
-    path("about/", views.show_about_page) #http://127.0.1:8000/about/ - about page
+    path("", views.render_home_main, name="home-page"), #http://127.0.1:8000/ - home page    
+    path("about/", views.show_about_page, name="about-page") #http://127.0.1:8000/about/ - about page
 ]
